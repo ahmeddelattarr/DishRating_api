e24f09e04c0bb7a2e6bf6723e6a858f399281475
Ahmed elattar <ahmeddelattarr@outlook.com>
2024-09-07 22:12:40 +0300

    optimizing  the models by using ORM

--- mealrater/api/models.py
+++ mealrater/api/models.py
@@ -3,42 +3,26 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models import Avg, Count
 
-#Meal(title, done
-# desc, done
-# numberof
-# rates,
-# avg rating)
-
 class Meal(models.Model):
-    title=models.CharField(max_length=32)
-    description=models.TextField(max_length=300)
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=300)
 
-    def number_of_rates(self):
-        rating= Rating.objects.filter(meal=self)
-        return len(rating)
+    def number_of_ratings(self):
+        return self.ratings.count()
 
-    def avg_rate(self):
-        sum=0
-        ratings=Rating.objects.filter(meal=self)
-
-        for rate in ratings:
-            sum+=rate.stars
-        if len(ratings)>0:
-            return sum/len(ratings)
-        else:
-            return 0
+    def avg_rating(self):
+        return self.ratings.aggregate(Avg('stars'))['stars__avg'] or 0
 
     def __str__(self):
         return self.title
 
-
 class Rating(models.Model):
-    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name='ratings')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     class Meta:
         unique_together = (('user', 'meal'),)
-        index_together = (('user', 'meal'),)
-
-
+        indexes = [
+            models.Index(fields=['user', 'meal']),
+        ]
