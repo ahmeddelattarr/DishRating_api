33eee2e87330869c2b6b69a28745d0591629c7b9
Ahmed elattar <ahmeddelattarr@outlook.com>
2024-09-10 05:49:30 +0300

    routing the urls and make the serializers.py more specific and making abasic viewsets for the api

--- mealrater/api/admin.py
+++ mealrater/api/admin.py
@@ -1,18 +1,15 @@
 from django.contrib import admin
-
-from .models import Meal, Rating
-
+from .models import Meal,Rating
 
 class RatingAdmin(admin.ModelAdmin):
-	list_display = ['id', 'meal', 'user', 'stars']
-	list_filter = ['meal', 'user']
-
+    list_display = ['id', 'meal', 'user', 'stars']
+    list_filter = ['meal', 'user']
 
 class MealAdmin(admin.ModelAdmin):
-	list_display = ['id', 'title', 'description']
-	search_fields = ['title', 'description']
-	list_filter = ['title', 'description']
-
+    list_display = ['id', 'title', 'description']
+    search_fields = ['title', 'description']
+    # Removed title and description from list_filter
+    # You can add other fields here if you have more suitable fields to filter by
 
 admin.site.register(Meal, MealAdmin)
 admin.site.register(Rating, RatingAdmin)
--- mealrater/api/serializers.py
+++ mealrater/api/serializers.py
@@ -2,13 +2,12 @@ from rest_framework import serializers
 from .models import Meal ,Rating
 
 class MealSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Meal
-		fields='__all__'
+    class Meta:
+        model = Meal
+        fields = ('id', 'title', 'description', 'number_of_ratings', 'avg_rating')
 
 class RatingSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Rating
-		fields='__all__'
-
+    class Meta:
+        model = Rating
+        fields = ('id', 'stars', 'user', 'meal')
 
