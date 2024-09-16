020c830b5104815133b69ba56ea262ee0a05e93e
Ahmed elattar <ahmeddelattarr@outlook.com>
2024-09-16 07:30:39 +0300

    formatting the code

--- mealrater/api/views.py
+++ mealrater/api/views.py
@@ -1,33 +1,34 @@
-
 from rest_framework import request, status, viewsets
 from rest_framework.views import APIView
-
-from .models import Meal, Rating
-from .serializers import MealSerializer, RatingSerializer,UserSerializer
-
 from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authtoken.models import Token
+
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated,AllowAny
-from rest_framework.authtoken.models import Token
+
+from .models import Meal, Rating
+from .serializers import MealSerializer, RatingSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
     def create(self, request, *args, **kwargs):
-        serializer=self.get_serializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        token,created=Token.objects.get_or_create(user=serializer.instance)
-        return Response({'token': token.key,},status=status.HTTP_201_CREATED)
+        token, created = Token.objects.get_or_create(user=serializer.instance)
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
 
     def list(self, request, *args, **kwargs):
-        response = {'message': 'You cant reach this part please register'}
+        response = {'message': 'You can’t reach this part, please register'}
         return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
 
 class MealViewSet(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
@@ -42,38 +43,34 @@ class MealViewSet(viewsets.ModelViewSet):
             user = request.user
 
             try:
-                # update
+                # Update rating
                 rating = Rating.objects.get(user=user, meal=meal)
                 rating.stars = stars
                 rating.save()
                 serializer = RatingSerializer(rating, many=False)
                 json = {
-                    'message': 'Meal Rate Updated',
+                    'message': 'Meal rating updated',
                     'result': serializer.data
                 }
                 return Response(json, status=status.HTTP_200_OK)
 
             except Rating.DoesNotExist:
-                # create
+                # Create new rating
                 rating = Rating.objects.create(stars=stars, meal=meal, user=user)
                 serializer = RatingSerializer(rating, many=False)
                 json = {
-                    'message': 'Meal Rate Created',
+                    'message': 'Meal rating created',
                     'result': serializer.data
                 }
                 return Response(json, status=status.HTTP_201_CREATED)
 
         else:
-            json = {
-                'message': 'stars not provided'
-            }
+            json = {'message': 'Stars not provided'}
             return Response(json, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-
-
-
