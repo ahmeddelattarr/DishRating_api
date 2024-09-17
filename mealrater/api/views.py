24284023fb61fa43d3c70bcb29c202c60f9a1ee3
Ahmed elattar <ahmeddelattarr@outlook.com>
2024-09-17 05:00:03 +0300

    Update views.py

--- mealrater/api/views.py
+++ mealrater/api/views.py
@@ -74,3 +74,26 @@ class RatingViewSet(viewsets.ModelViewSet):
     serializer_class = RatingSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if username is None or password is None:
+            return Response({'error': 'Please provide both username and password.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
